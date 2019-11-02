f11b23e25492f799b6334dd39d297521459fe9c7
AliVillegas <30273464+AliVillegas@users.noreply.github.com>
2019-11-01 17:28:26 -0700

    Final1

--- Testing/ScriptInterfaz.m
+++
@@ -1,6 +0,0 @@
-           import mlreportgen.dom.*
-           D=Document('FromTemplate','docx','Reporte.dotx')
-           open(D)
-           moveToNextHole(D)
-           append(D, "Hello World chido")
-           close(D)
